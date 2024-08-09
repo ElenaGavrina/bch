dec126bf7092a836ebdee5d95726bc9e74e53c28
elena <gavrinaelenav@gmail.com>
2024-08-10 01:26:38 +0300

    update main

--- main.go
+++ main.go
@@ -1,20 +1,60 @@
 package main
 
-import "time"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
 type Block struct {
-	PrevHash  string
-	Hash      string
-	Timestamp time.Time
-	Data      string
+	PrevHash  []byte
+	Hash      []byte
+	Data      []byte
 }
 
 type Blockchain struct {
-	bch []*Block
+	blocks []*Block
 } 
 
-func NewBlock() *Block {
-	return &Block{
-		
+func(b *Block) DeriveHash() {
+	sum := bytes.Join([][]byte{[]byte(b.Data), []byte(b.PrevHash)}, []byte{})
+	hash :=sha256.Sum256(sum)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+		PrevHash: prevHash,
+		Data: []byte(data),
+	}
+
+	block.DeriveHash()
+	return block
+}
+
+func(chain *Blockchain) AddBlock (data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data,prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *Blockchain {
+	return &Blockchain{[] *Block{Genesis()}}
+}
+
+func main() {
+	chain := InitBlockChain()
+	chain.AddBlock("First block")
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous hash: %x\n",block.PrevHash)
+		fmt.Printf("Hash: %x\n",block.Hash)
+		fmt.Printf("Dats: %s\n",block.Data)
 	}
 }
