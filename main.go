3bb9c26a36e5dd9e26e1fc7ffc437bebdd63c1dc
elena <gavrinaelenav@gmail.com>
2024-08-19 19:57:22 +0300

    update main

--- main.go
+++ main.go
@@ -1,60 +1,103 @@
 package main
 
 import (
-	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"strconv"
+	"time"
 )
 
 type Block struct {
-	PrevHash  []byte
-	Hash      []byte
-	Data      []byte
+	Pos       int
+	Hash      string
+	PrevHash  string
+	Timestamp string
+	Data      BookCheckout
 }
 
 type Blockchain struct {
 	blocks []*Block
-} 
-
-func(b *Block) DeriveHash() {
-	sum := bytes.Join([][]byte{[]byte(b.Data), []byte(b.PrevHash)}, []byte{})
-	hash :=sha256.Sum256(sum)
-	b.Hash = hash[:]
 }
 
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{
-		PrevHash: prevHash,
-		Data: []byte(data),
-	}
+type Book struct {
+	ID          string 
+	ISBN        string 
+	Title       string 
+	Author      string 
+	PublishDate string 
+}
 
-	block.DeriveHash()
+type BookCheckout struct {
+	BookID       string 
+	User         string
+	CheckoutDate string 
+	IsGenesis    bool   
+}
+
+func (b *Block) generateHash() {
+	bytes, _ := json.Marshal(b.Data)
+
+	data := strconv.Itoa(b.Pos) + b.Timestamp + string(bytes)
+
+	hash := sha256.New()
+	hash.Write([]byte(data))
+	b.Hash = hex.EncodeToString(hash.Sum(nil))
+}
+
+func (b *Block) validateHash(hash string) bool {
+	b.generateHash()
+	return b.Hash == hash
+}
+
+func (bc *Blockchain) AddBlock(data BookCheckout) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	block := CreateBlock(prevBlock, data)
+	if validBlock(block, prevBlock) {
+		bc.blocks = append(bc.blocks, block)
+	}
+}
+
+func CreateBlock(prevBlock *Block, data BookCheckout) *Block {
+	block := new(Block)
+	block.Pos = prevBlock.Pos + 1
+	block.Data = data
+	block.Timestamp = time.Now().String()
+	block.PrevHash = prevBlock.Hash
+	block.generateHash()
+
 	return block
 }
 
-func(chain *Blockchain) AddBlock (data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data,prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
+func validBlock(block *Block, prevBlock *Block) bool {
+	if block.PrevHash != prevBlock.Hash {
+		return false
+	}
+	if !block.validateHash(block.Hash) {
+		return false
+	}
+	if prevBlock.Pos+1 != block.Pos {
+		return false
+	}
+	return true
 }
 
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
+func GenesisBlock() *Block {
+	return CreateBlock(new(Block), BookCheckout{IsGenesis: true})
 }
 
-func InitBlockChain() *Blockchain {
-	return &Blockchain{[] *Block{Genesis()}}
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{GenesisBlock()}}
 }
 
 func main() {
-	chain := InitBlockChain()
-	chain.AddBlock("First block")
-	chain.AddBlock("Second block")
-	chain.AddBlock("Third block")
-
-	for _, block := range chain.blocks {
-		fmt.Printf("Previous hash: %x\n",block.PrevHash)
-		fmt.Printf("Hash: %x\n",block.Hash)
-		fmt.Printf("Dats: %s\n",block.Data)
+	blockchain := NewBlockchain()
+	for _, block := range blockchain.blocks {
+		fmt.Printf("Prev.hash: %x\n", block.PrevHash)
+		bytes, _ := json.MarshalIndent(block.Data, "", " ")
+		fmt.Printf("Data:%v\n", string(bytes))
+		fmt.Printf("Hash:%x\n", block.Hash)
+		fmt.Println()
 	}
 }
